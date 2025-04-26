504e5f9f10583cdc4b082565f66faae861d3d6ff
Rohyit <rathodr070@gmail.com>
2025-04-27 01:18:17 +0530

    Solved Assign Cookies Problem

---
+++ Greedy/Assign Cookies.java
@@ -0,0 +1,23 @@
+// Problem: Assign Cookies
+// Link: https://leetcode.com/problems/assign-cookies/
+// Approach: Greedy 
+
+package Greedy;
+import java.util.*;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        int l=0 , r = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(l<n && r<m){
+            if(g[l]<=s[r]){
+                l++;
+            }
+            r++;
+        }
+        return l;
+    }
+}
