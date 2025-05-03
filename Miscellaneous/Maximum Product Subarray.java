3f3f865e950c4ac31cad352ec6d5bccb2681b953
Rohyit <rathodr070@gmail.com>
2025-05-03 14:16:56 +0530

    Solved Maximum Product Subarray Problem

---
+++ Miscellaneous/Maximum Product Subarray.java
@@ -0,0 +1,21 @@
+// Problem: Maximum Product Subarray
+// Link: https://leetcode.com/problems/maximum-product-subarray/description/
+
+package Miscellaneous;
+
+// Variation of Kadane's Algorithm
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int currMax = nums[0], currMin = nums[0], maxProd = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            int temp = currMax;
+            currMax = Math.max(nums[i], Math.max(currMax * nums[i], currMin * nums[i]));
+            currMin = Math.min(nums[i], Math.min(temp * nums[i], currMin * nums[i]));
+        
+            maxProd = Math.max(maxProd, currMax);
+        }  
+        return maxProd;
+    }
+}
