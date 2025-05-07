11b4f17168a44c57f401fdddf0fd5ddc62624b93
Rohyit <rathodr070@gmail.com>
2025-05-07 14:13:05 +0530

    Solved Find the Difference Problem

---
+++ Miscellaneous/Find the Difference.java
@@ -0,0 +1,25 @@
+// Problem: Find the Difference
+// Link: https://leetcode.com/problems/find-the-difference/
+
+package Miscellaneous;
+import java.util.*;
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        
+        for(char c : s.toCharArray()){
+            if(!map.containsKey(c)){
+                return c;
+            }
+            map.put(c, map.get(c)-1);
+            if(map.get(c)==0){
+                map.remove(c);
+            }
+        }
+        return map.keySet().iterator().next();
+    }
+}
