3b0e9e36e8736b512fa4b4e79ebd3673e912c3e0
Rohyit <rathodr070@gmail.com>
2025-05-08 15:10:12 +0530

    Solved Largest Odd Number in a String Problem

---
+++ Miscellaneous/Largest Odd Number in a String.java
@@ -0,0 +1,15 @@
+// Problem: Largest Odd Number in a String
+// Link: https://leetcode.com/problems/largest-odd-number-in-string/
+
+package Miscellaneous;
+
+class Solution {
+    public String largestOddNumber(String num) {
+        for(int i=num.length()-1; i>=0; i--){
+            if(num.charAt(i)=='1' || num.charAt(i)=='3' || num.charAt(i)=='5' || num.charAt(i)=='7' || num.charAt(i)=='9' ){
+                return num.substring(0, i+1);
+            }
+        }
+        return "";
+    }
+}
