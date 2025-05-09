b6537842df02d10ce878bf070a5e0886d34180f5
Rohyit <rathodr070@gmail.com>
2025-05-09 21:04:54 +0530

    Solved Set Mismatch Problem

---
+++ Hashing/Set Mismatch.java
@@ -0,0 +1,30 @@
+// Problem: Set Mismatch
+// Link: https://leetcode.com/problems/set-mismatch/
+// Approach: Hashing
+
+package Hashing;
+import java.util.*;
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int n = nums.length;
+        int duplicate = -1;
+        for(int i=0; i<n; i++){
+            if(set.contains(nums[i])){
+                duplicate = nums[i];
+            }
+            set.add(nums[i]);
+        }
+    
+        int missing = -1;
+
+        for(int i=1; i<=n; i++){
+            if(!set.contains(i)){
+                missing = i;
+                break;
+            }
+        }
+        return new int[]{duplicate, missing};
+    }
+}
