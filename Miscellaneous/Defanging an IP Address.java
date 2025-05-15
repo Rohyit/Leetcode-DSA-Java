1b499516b18307b74d0746637576665d9d2ac0f2
Rohyit <rathodr070@gmail.com>
2025-05-15 12:30:23 +0530

    Solved Defanging an IP Address Problem

---
+++ Miscellaneous/Defanging an IP Address.java
@@ -0,0 +1,19 @@
+// Problem: Defanging an IP Address
+// Link: https://leetcode.com/problems/defanging-an-ip-address/
+
+package Miscellaneous;
+
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<address.length(); i++){
+            if(address.charAt(i) == '.'){
+                sb.append("[.]");
+            }
+            else{
+                sb.append(address.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
--- Miscellaneous/Height Checker.java
+++ Miscellaneous/Height Checker.java
@@ -10,7 +10,7 @@ class Solution {
         for(int i=0; i<heights.length; i++){
             original[i] = heights[i];
         }
-        Arrays.sort(heights);
+        Arrays.sort(heights); 
         int count = 0;
         for(int i=0; i<heights.length; i++){
             if(heights[i] != original[i]){
