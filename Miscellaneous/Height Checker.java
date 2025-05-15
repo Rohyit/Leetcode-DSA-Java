12f91c20f9b39730ba2cbb399b582e8be02d3dc6
Rohyit <rathodr070@gmail.com>
2025-05-15 12:13:29 +0530

    Solved Height Checker Problem

---
+++ Miscellaneous/Height Checker.java
@@ -0,0 +1,22 @@
+// Problem: Height Checker
+// Link: https://leetcode.com/problems/height-checker/description/
+
+package Miscellaneous;
+import java.util.*;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] original = new int[heights.length];
+        for(int i=0; i<heights.length; i++){
+            original[i] = heights[i];
+        }
+        Arrays.sort(heights);
+        int count = 0;
+        for(int i=0; i<heights.length; i++){
+            if(heights[i] != original[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
