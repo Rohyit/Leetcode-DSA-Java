feaff27b6482e6151e6817e70fb75e21b2e474e9
Rohyit <rathodr070@gmail.com>
2025-05-17 15:35:08 +0530

    Solved POTD : Sort Colors - 17-May-2025

---
+++ POTD/Sort Colors.java
@@ -0,0 +1,28 @@
+// Problem: Sort Colors
+// Link: https://leetcode.com/problems/sort-colors/description/
+// Date : 17-May-2025
+
+package POTD;
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int red=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                int temp = nums[red];
+                nums[red] = nums[i];
+                nums[i] = temp;
+                red++;
+            }
+        }
+        int white = red;
+        for(int i=white; i<nums.length; i++){
+            if(nums[i]==1){
+                int temp = nums[white];
+                nums[white] = nums[i];
+                nums[i] = temp;
+                white++;
+            }
+        }
+    }
+}
