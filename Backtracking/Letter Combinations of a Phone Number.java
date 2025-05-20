c456f3c151aaa4212072c2dc867b0f8c940f4ff8
Rohyit <rathodr070@gmail.com>
2025-05-20 18:40:37 +0530

    Solved Letter Combinations of a Phone Number Problem

---
+++ Backtracking/Letter Combinations of a Phone Number.java
@@ -0,0 +1,42 @@
+// Problem: Letter Combinations of a Phone Number
+// Link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+// Approach: Using Backtracking
+
+package Backtracking;
+import java.util.*;
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+
+        if(digits.length() == 0 || digits == null){
+            return list;
+        }
+
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+
+        backtrack(digits, 0, new StringBuilder(), list, map);
+        return list;
+    }
+    public void backtrack(String digits, int idx, StringBuilder ds, List<String> list, Map<Character, String> map){
+        if(idx == digits.length()){
+            list.add(ds.toString());
+            return;
+        }
+
+        String letters = map.get(digits.charAt(idx));
+        for(char c : letters.toCharArray()){
+            ds.append(c);
+            backtrack(digits, idx+1, ds, list, map);
+            ds.deleteCharAt(ds.length()-1);
+        }
+    }
+}
