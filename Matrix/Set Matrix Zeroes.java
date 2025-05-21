a67d718c217b152599ccf93cc960327e636833f1
Rohyit <rathodr070@gmail.com>
2025-05-21 18:52:31 +0530

    Solved Set Matrix Zeroes Problem

---
+++ Matrix/Set Matrix Zeroes.java
@@ -0,0 +1,33 @@
+// Problem: Set Matrix Zeroes
+// Link: https://leetcode.com/problems/set-matrix-zeroes/description/
+// Aprroach : Matrix
+
+package Matrix;
+import java.util.*;
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for(int r : row){
+            for(int i=0; i<n; i++){
+                matrix[r][i] = 0;
+            }
+        }
+        for(int c : col){
+            for(int i=0; i<m; i++){
+                matrix[i][c] = 0;
+            }
+        }
+    }
+}
