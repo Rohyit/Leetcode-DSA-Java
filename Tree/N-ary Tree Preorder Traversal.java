3f5a5ba5709b54e55fac8061e0bd71a802a80394
Rohyit <rathodr070@gmail.com>
2025-05-29 13:22:54 +0530

    Solved N-ary Tree Preorder Traversal Problem

---
+++ Tree/N-ary Tree Preorder Traversal.java
@@ -0,0 +1,36 @@
+// Problem: N-ary Tree Preorder Traversal
+// Link: N-ary Tree Preorder Traversal
+// Approach: Tree
+
+package Tree;
+import java.util.*;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        if(root==null){
+            return list;
+        }
+        list.add(root.val);
+        for(Node node : root.children){
+            preorder(node);
+        }
+        return list;
+    }
+}
