b335a026a32134b5b14f19857a0be18b8c6a5d96
Rohyit <rathodr070@gmail.com>
2025-06-09 18:12:11 +0530

    Solved Remove Duplicates from Sorted Array II Problem

---
+++ Two Pointers/Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,21 @@
+// Problem: Remove Duplicates from Sorted Array II
+// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+// Approach: Two pointer
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(n <= 2){
+            return n;
+        }
+        int slow = 2;
+        for(int fast = 2; fast < n ; fast++){
+            if(nums[fast] != nums[slow - 2]){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow; 
+    }
+}
