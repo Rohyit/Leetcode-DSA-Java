843fd66ed76b186457aa8bb694e688b92e821a48
Rohyit <rathodr070@gmail.com>
2025-07-20 16:07:52 +0530

    Solved Minimum Cost to Cut a Stick Problem

--- Dynamic Programming/Minimum Cost to Cut a Stick - Memoization.java
+++ Dynamic Programming/Minimum Cost to Cut a Stick - Memoization.java
@@ -7,29 +7,31 @@ import java.util.*;
 class Solution {
     public int minCost(int n, int[] cuts) {
         int len = cuts.length;
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>(); // We will have to add 0 and n to the array before using recursion.
         list.add(0);
         for(int i=0; i<len; i++){
             list.add(cuts[i]);
         }
         list.add(n);
 
-        int[] arr = new int[len+2];
+        int[] arr = new int[len+2]; // this is the new hypothetical array 
         for(int i=0; i<len+2; i++){
             arr[i] = list.get(i);
         }
         int length = arr.length;
-        Arrays.sort(arr);
-        
+        Arrays.sort(arr); // sorting the array first is important to use partition DP in order to solve subproblems independently
+
         int[][] dp = new int[length][length];
         for(int i=0; i<length; i++){
             Arrays.fill(dp[i], -1);
         }
 
-        return minCost(1, length-2, arr, dp);
+        return minCost(1, length-2, arr, dp); // length-2 is basically n-1 in original array.
     }
     private int minCost(int i, int j, int[] cuts, int[][] dp){
-
+        int n = cuts.length;
+        
+        // Base Case
         if(i>j){
             return 0;
         }
@@ -40,6 +42,8 @@ class Solution {
 
         int mini = Integer.MAX_VALUE;
         for(int ind = i; ind <=j; ind++){
+
+            // cost if the stick is cut at ind is : cuts[j+1] - cuts[i-1]
             int cost = cuts[j+1] - cuts[i-1] + minCost(i, ind - 1, cuts, dp) + minCost(ind + 1, j, cuts, dp);
             mini = Math.min(cost, mini);
         }
