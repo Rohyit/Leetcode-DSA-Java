71e053883f818d58453e2fb015206992eac13c69
Rohyit <rathodr070@gmail.com>
2025-07-21 18:08:07 +0530

    Solved Binary Search Problem

---
+++ Binary Search/Binary Search.java
@@ -0,0 +1,25 @@
+// Problem: Binary Search
+// Link: https://leetcode.com/problems/binary-search/description/
+// Approach: Binary Search
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int mid = -1;
+        while(low <= high){
+            mid = (low + high) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
