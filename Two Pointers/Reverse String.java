c34db3433a792da57dbb2c73fcbff26fa104d35b
Rohyit <rathodr070@gmail.com>
2025-08-19 11:14:30 +0530

    Updated TC & SC

--- Two Pointers/Reverse String.java
+++ Two Pointers/Reverse String.java
@@ -2,6 +2,9 @@
 // Link: https://leetcode.com/problems/reverse-string/description/
 // Approach: Two pointer
 
+// T.C. : O(n)
+// S.C. : O(1)
+
 class Solution {
     public void reverseString(char[] s) {
         int left = 0, right = s.length-1;
