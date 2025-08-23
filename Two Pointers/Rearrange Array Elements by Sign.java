1ca1373720d7b0d037f92b4a49dc1896160970aa
Rohyit <rathodr070@gmail.com>
2025-08-23 19:35:56 +0530

    Updated TC & SC

--- Two Pointers/Rearrange Array Elements by Sign.java
+++ Two Pointers/Rearrange Array Elements by Sign.java
@@ -2,28 +2,26 @@
 // Link: https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
 // Approach: Two pointer
 
+// T.C. : O(n)
+// S.C. : O(n)
+
 import java.util.*;
 
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int[] arr = new int[nums.length];
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        
-        for(int num : nums){
-            if(num>0){
-                list1.add(num);
-            }
-            else{
-                list2.add(num);
+        int pos = 0, neg = 1;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] > 0){
+                arr[pos] = nums[i];
+                pos += 2;
             }
         }
-        int pos=0, neg=1;
-        for(int i=0; i<list1.size(); i++){
-            arr[pos] = list1.get(i);
-            arr[neg] = list2.get(i);
-            pos+=2;
-            neg+=2;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] < 0){
+                arr[neg] = nums[i];
+                neg += 2;
+            }
         }
         return arr;
     }
