8a023477394e7054717a5b251dd89307f940ff93
Rohyit <rathodr070@gmail.com>
2025-09-07 16:45:10 +0530

    Updated TC & SC

--- Sliding Window/Maximum Subarray.java
+++ Sliding Window/Maximum Subarray.java
@@ -1,19 +1,23 @@
 // Problem: Maximum Subarray
 // Link: https://leetcode.com/problems/max-consecutive-ones/
-// Approach: Sliding Window
+// Approach: Prefix Sum
+
+// T.C. : O(n)
+// S.C. : O(1)
 
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int maxCount = Integer.MIN_VALUE, count = 0;
-        for(int right=0; right<nums.length; right++){
-            if(nums[right]==1){
-                count++;
+        int maxSum= Integer.MIN_VALUE, prefixSum=0;
+        for(int num: nums){
+            prefixSum+=num;
+            
+            if(prefixSum>maxSum){
+                maxSum=prefixSum;
             }
-            else{
-                maxCount = Math.max(maxCount, count);
-                count=0;
+            if(prefixSum<0){
+                prefixSum=0;
             }
         }
-        return Math.max(maxCount, count);
+        return maxSum;
     }
 }
