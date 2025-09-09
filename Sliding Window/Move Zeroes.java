801b85a219d634f8ae0b02ec411590301cb950d9
Rohyit <rathodr070@gmail.com>
2025-09-09 16:55:42 +0530

    Updated TC & SC

--- Sliding Window/Move Zeroes.java
+++ Sliding Window/Move Zeroes.java
@@ -2,6 +2,9 @@
 // Link: https://leetcode.com/problems/move-zeroes/description/
 // Approach: Sliding Window
 
+// T.C. : O(n)
+// S.C. : O(1)
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int left=0;
