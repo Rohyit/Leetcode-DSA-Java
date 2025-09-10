56edc10b06507d64ac5693fbc9496a8f51e0e4c6
Rohyit <rathodr070@gmail.com>
2025-09-10 19:28:43 +0530

    Updated TC & SC

--- Sliding Window/Max Consecutive Ones.java
+++ Sliding Window/Max Consecutive Ones.java
@@ -2,6 +2,9 @@
 // Link: https://leetcode.com/problems/max-consecutive-ones/description/
 // Approach: Sliding Window
 
+// T.C. : O(n)
+// S.C. : O(1)
+
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int maxCount = Integer.MIN_VALUE, count = 0;
