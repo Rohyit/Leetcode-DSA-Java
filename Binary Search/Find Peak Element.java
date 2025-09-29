8f6d2428d0188340bb5958a9fdf1ec8332e600de
Rohyit <rathodr070@gmail.com>
2025-09-29 20:19:45 +0530

    Updated TC & SC

--- Binary Search/Find Peak Element.java
+++ Binary Search/Find Peak Element.java
@@ -1,9 +1,33 @@
 // Problem: Find Peak Element
 // Link: https://leetcode.com/problems/find-peak-element/description/
-// Approach: Binary Search 
+// Approach: Binary Search
 
 class Solution {
     public int findPeakElement(int[] nums) {
+        /*
+        
+        // BRUTE FORCE : O(n)
+
+        int n = nums.length;
+        if(n==1){
+            return 0;
+        }
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[n-1] > nums[n-2]){
+            return n-1;
+        }
+        for(int i=1; i<n-1; i++){
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]){
+                return i;
+            }
+        }
+        return -1;
+        */
+        
+        // OPTIMIZED : O(logn)
+        
         int n = nums.length;
         if(n == 1){
             return 0;
