2a2e778792ce133af8439550bd56bc9a1ccaa4ca
Rohyit <rathodr070@gmail.com>
2025-09-30 15:24:56 +0530

    Updated TC & SC

--- Hashing/Find the Duplicate Number.java
+++ Hashing/Find the Duplicate Number.java
@@ -1,12 +1,16 @@
 // Problem: Find the Duplicate Number
 // Link: https://leetcode.com/problems/find-the-duplicate-number/description/
-// Approach: Hashing
-package Hashing;
-import java.util.*;
+// Approach: Tortoise and Hare Algo
 
+// T.C. : O(n)
+// S.C. : O(1)
+
+package Hashing;
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<Integer>();
+
+        // BRUTE FORCE : T.C. : O(n) & S.C. : O(n) 
+        /*Set<Integer> set = new HashSet<Integer>();
         for(int num : nums){
             if(set.contains(num)){
                 return num;
@@ -14,5 +18,25 @@ class Solution {
             set.add(num);
         }
         return -1;
+        */
+
+        // OPTIMIZED : T.C. : O(n) & S.C. : O(1)
+        int n = nums.length - 1;
+        int slow = 0;
+        int fast = 0;
+        
+        do{
+            slow = nums[slow];
+            fast = nums[fast];
+            fast = nums[fast];
+        } while(slow != fast);
+
+        slow = 0;
+
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 }
