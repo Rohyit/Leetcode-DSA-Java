3ba0f28cab22fb4610e903ef10ed9e9686b4299e
Rohyit <rathodr070@gmail.com>
2025-10-04 23:29:42 +0530

    Updated TC & SC

--- Two Pointers/2Sum.java
+++ Two Pointers/2Sum.java
@@ -7,6 +7,20 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        /*
+        // BRUTE FORCE - T.C. : O(n^2)
+            int n = nums.length;
+            for(int i=0; i<n-1; i++){
+                for(int j=i+1; j<n; j++){
+                    if(nums[i] + nums[j] == target){
+                        return new int[]{i+1, j+1};
+                    }
+                }
+            }
+            return new int[]{};
+        */
+        // OPTIMIZED
+
         int i=1;
         int j=nums.length;
         while(i<j){
@@ -23,4 +37,3 @@ class Solution {
         return new int[]{};
     }
 }
-
