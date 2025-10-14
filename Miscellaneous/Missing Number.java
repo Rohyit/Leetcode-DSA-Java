9b5c35bccae7c05276d10958fd3243631f462183
Rohyit <rathodr070@gmail.com>
2025-10-14 22:29:29 +0530

    Updated TC & SC

--- Miscellaneous/Missing Number.java
+++ Miscellaneous/Missing Number.java
@@ -9,11 +9,11 @@ package Miscellaneous;
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int expectedSum = n*(n+1)/2;
+        int total = n*(n+1)/2;
         int sum = 0;
         for(int i=0; i<n; i++){
             sum += nums[i];
         }
-        return expectedSum - sum;
+        return total - sum;
     }
 }
