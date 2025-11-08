5097d00ab3a95133630549ec40ce4c3abc836610
Rohyit <rathodr070@gmail.com>
2025-11-08 23:14:46 +0530

    Solved Longest Common Palindrome Problem

---
+++ POTD/Longest Common Prefix.java
@@ -0,0 +1,25 @@
+// Problem: Longest Common Prefix
+// Link: https://leetcode.com/problems/longest-common-prefix/
+
+import java.util.*;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        int n = strs.length;
+        String s1 = strs[0];
+        String s2 = strs[n-1];
+
+        int ind = 0;
+
+        while(ind<s1.length() && ind<s2.length()){
+            if(s1.charAt(ind) == s2.charAt(ind)){
+                ind++;
+            }
+            else{
+                break;
+            }
+        }
+        return s1.substring(0,ind);
+    }
+}
