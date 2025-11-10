3103e5034946e3c7844b6ef1a3f78570e71b8d96
Rohyit <rathodr070@gmail.com>
2025-11-10 23:25:50 +0530

    Updated TC and SC

--- Bit Manipulation/Single Number.java
+++ Bit Manipulation/Single Number.java
@@ -2,6 +2,9 @@
 // Link: https://leetcode.com/problems/single-number/description/
 // Approach: Bit Manipulation
 
+// T.C. : O(n)
+// S.C. : O(1)
+
 class Solution {
     public int singleNumber(int[] nums) {
         int xor = 0;
