7cf96e07e877945542b3449b54ed4c078bc3173a
Rohyit <rathodr070@gmail.com>
2025-11-24 19:33:04 +0530

    Solved Ugly Number Problem

---
+++ Miscellaneous/Ugly Number.java
@@ -0,0 +1,15 @@
+// Problem: Ugly Number
+// Link: https://leetcode.com/problems/ugly-number/description/
+
+package Miscellaneous;
+
+class Solution {
+    public boolean isUgly(int n) {
+        for(int i=2; i<6 && n > 0; i++){
+            while(n % i == 0){
+                n /= i;
+            }
+        }
+        return n == 1;
+    }
+}
